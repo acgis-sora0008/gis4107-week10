37289d6d49f2747352b9cf631e76cf5101f8784d
Brigitte Hanna <hann0327@algonquinlive.com>
2023-11-08 13:35:53 -0500

    Initial commit

--- MousmiS_BrigitteH/file_utils.py
+++ MousmiS_BrigitteH/file_utils.py
@@ -1,20 +1,16 @@
 def get_file_content(file_name):
     try:
-        # file_name = r"data\demo.txt"
         with open (file_name) as infile:
             contents = infile.read()
-            # print(contents)
         return contents
     except FileNotFoundError:
         return "ERROR"
         
 def write_to_file(file_name, content):
     """This function will not return anything
-
     Args:
         file_name (string): _description_
-        content (string): _description_
-        
+        content (string): _description_    
     """
     file_name = r"data\TestFile.txt"
     with open (file_name,'w') as outfile:
--- MousmiS_BrigitteH/test_file_utils.py
+++ MousmiS_BrigitteH/test_file_utils.py
@@ -1,8 +1,5 @@
 import file_utils as fu
 import os
-import csv
-import data
-
 
 def test_get_file_content_case01():
     script_folder = os.path.dirname(os.path.abspath(__file__))
@@ -11,24 +8,17 @@ def test_get_file_content_case01():
     actual = fu.get_file_content(r'data\demo.txt')
     assert expected == actual
     
-    # test_get_file_content_case01()
-    
 def test_get_file_content_case02():
     script_folder = os.path.dirname(os.path.abspath(__file__))
     file_name = os.path.join(script_folder, "mousmi", "mousmi.txt")
-    expected = "ERROR"
-    
-     
-    actual = fu.get_file_content('mousmi.txt')
-    
+    expected = "ERROR" 
+    actual = fu.get_file_content(file_name)
     assert expected == actual    
     
 def test_write_to_file():
     script_folder = os.path.dirname(os.path.abspath(__file__))
     file_name = os.path.join(script_folder,"data", "TestFile.txt")
     content = "Hello World"
-    
     fu.write_to_file(file_name, content)
     expected = fu.get_file_content(file_name)
-    
     assert expected == content  
